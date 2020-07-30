d51f50824f5aefeb606475f53dc6257bf4a1130c
fidhiaaka <vidhiaaka7@gmail.com>
2020-07-30 21:57:38 +0700

    https://github.com/fidhiaaka/oop.git

---
+++ Frog.php
@@ -0,0 +1,11 @@
+<?php
+
+  class Frog extends Animal 
+  {
+    public function jump()
+    {
+      echo "hop hop" . "<br>";
+    }
+  }
+
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+  require_once 'animal.php';
+  require_once 'Frog.php';
+  require_once 'Ape.php';
+
+  $sheep = new Animal("shaun");
+
+  echo $sheep->name . "<br>"; // "shaun"
+  echo $sheep->legs . "<br>"; // 2
+  echo $sheep->cold_blooded . "<br>"; // false
+
+  // NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
+  // index.php
+  $sungokong = new Ape("kera sakti");
+  $sungokong->yell(); // "Auooo"
+
+  $kodok = new Frog("buduk");
+  $kodok->jump() ; // "hop hop"
+
+?>
